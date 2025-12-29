fe56ee74a0fa56f67d99270eed0f4c8271984e0f
nchao03 <62034613+ChooChooChao@users.noreply.github.com>
2025-12-29 11:07:52 -0800

    Completed Preparing the Data and Creating Chroma DB
    Following along in the video, these are the completed sections of "Preparing the Data" and "Creating the Chroma DB". Created the data folder to hold a md version of the Alice's Adventures in Wonderland I got from online and then parsed the data to chunks. Then using those chunks saved it to a new chroma db. Main changes made include changing to local embeddings using sentence-transformers due to OpenAI APIs requirement for credits.

---
+++ create_database.py
@@ -0,0 +1,69 @@
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+from langchain_community.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
+
+import os
+import shutil
+
+
+DATA_PATH = "data/books"
+CHROMA_PATH = "chroma"
+
+def main():
+    generate_data_store()
+
+def generate_data_store():
+    documents = load_documents()
+    chunks = split_text(documents)
+    save_to_chroma(chunks)
+
+
+def load_documents():
+    loader = DirectoryLoader(
+        DATA_PATH,
+        glob="*.md",
+        loader_cls=TextLoader,
+        loader_kwargs={"encoding": "utf-8"},
+    )
+    documents = loader.load()
+    return documents
+
+def split_text(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 1000,
+
+        chunk_overlap = 500,
+        length_function = len,
+        add_start_index = True,
+    )
+
+    chunks = text_splitter.split_documents(documents)
+    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+
+    document = chunks[10]
+    print(document.page_content)
+    print(document.metadata)
+
+    return chunks
+
+def save_to_chroma(chunks:  list[Document]):
+    # clear db first
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    # create new db from documents
+    db = Chroma.from_documents(
+        chunks, embeddings, persist_directory = CHROMA_PATH
+    )
+    db.persist() # force save
+    print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}")
+    
+if __name__ == "__main__":
+    main()
+
